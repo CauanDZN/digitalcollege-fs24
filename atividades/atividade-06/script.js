a6c92041cbd2cc51c20e727d7e31871f6bafe5bd
Cauan <cauanvictor0325@gmail.com>
2023-10-21 16:12:59 -0300

    feat: adding challenge 06

---
+++ atividades/atividade-06/script.js
@@ -0,0 +1,10 @@
+let resposta = prompt("Qual a sua idade?")
+
+if (resposta >= 18) {
+    alert("Você é maior de idade! Pode dirigir.")
+    console.log(resposta)
+} else {
+    alert("Você é menor de idade! Não pode dirigir.")
+    console.log(resposta)
+}
+
